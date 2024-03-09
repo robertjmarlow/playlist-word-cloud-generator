969e63f7af781fdb6fd5ab5368ca261a3fe93de1
robertjmarlow <robertjmarlow@gmail.com>
2024-03-08 19:16:19 -0600

    Adding spotless

--- src/main/java/com/marlowsoft/playlistwordcloudgenerator/PlaylistwordcloudgeneratorApplication.java
+++ src/main/java/com/marlowsoft/playlistwordcloudgenerator/PlaylistwordcloudgeneratorApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlaylistwordcloudgeneratorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlaylistwordcloudgeneratorApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(PlaylistwordcloudgeneratorApplication.class, args);
+  }
 }
