ab594e3cf2670ffc6b63d58b8a4c64b85a824657
robertjmarlow <robertjmarlow@gmail.com>
2024-03-08 19:09:43 -0600

    Initial commit from Spring Initializr

---
+++ src/main/java/com/marlowsoft/playlistwordcloudgenerator/PlaylistwordcloudgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.marlowsoft.playlistwordcloudgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaylistwordcloudgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaylistwordcloudgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marlowsoft/playlistwordcloudgenerator/PlaylistwordcloudgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marlowsoft.playlistwordcloudgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaylistwordcloudgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
