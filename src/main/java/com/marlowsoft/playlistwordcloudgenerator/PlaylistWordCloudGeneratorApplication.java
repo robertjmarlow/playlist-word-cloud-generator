76a337460b4680043068e10f4e5a2d5bc6b345bd
robertjmarlow <robertjmarlow@gmail.com>
2024-03-18 17:53:04 -0500

    Well something got generated lol

--- src/main/java/com/marlowsoft/playlistwordcloudgenerator/PlaylistWordCloudGeneratorApplication.java
+++ src/main/java/com/marlowsoft/playlistwordcloudgenerator/PlaylistWordCloudGeneratorApplication.java
@@ -1,5 +1,11 @@
 package com.marlowsoft.playlistwordcloudgenerator;
 
+import com.kennycason.kumo.CollisionMode;
+import com.kennycason.kumo.WordCloud;
+import com.kennycason.kumo.bg.CircleBackground;
+import com.kennycason.kumo.font.scale.SqrtFontScalar;
+import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+import com.kennycason.kumo.palette.ColorPalette;
 import com.marlowsoft.playlistwordcloudgenerator.lyrics.LyricsRetriever;
 import com.marlowsoft.playlistwordcloudgenerator.lyrics.genius.obj.ImmutableLyricsRequest;
 import com.marlowsoft.playlistwordcloudgenerator.lyrics.genius.obj.ImmutableLyricsRequestTrack;
@@ -7,6 +13,8 @@ import com.marlowsoft.playlistwordcloudgenerator.lyrics.genius.obj.LyricsRequest
 import com.marlowsoft.playlistwordcloudgenerator.lyrics.genius.obj.LyricsResponse;
 import com.marlowsoft.playlistwordcloudgenerator.playlist.PlaylistRetriever;
 import com.marlowsoft.playlistwordcloudgenerator.playlist.spotify.obj.Playlist;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -50,12 +58,35 @@ public class PlaylistWordCloudGeneratorApplication {
                   .build());
         }
 
-        final LyricsResponse response = lyricsRetriever.getLyrics(lyricsRequestBuilder.build());
-        final List<String> lyrics = response.getLyrics();
+        final List<String> lyrics =
+            lyricsRetriever.getLyrics(lyricsRequestBuilder.build()).getLyrics();
 
         LOGGER.info("i got these lyrics back: {}", lyrics);
 
+        // generate the word cloud
+        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+        final WordCloud wordCloud = getWordCloud();
+        wordCloud.build(frequencyAnalyzer.load(lyrics));
+        wordCloud.writeToFile("build/wordcloud_circle_sqrt_font.png");
+
         System.exit(SpringApplication.exit(configurableApplicationContext));
+      }
+
+      private static WordCloud getWordCloud() {
+        final WordCloud wordCloud =
+            new WordCloud(new Dimension(600, 600), CollisionMode.PIXEL_PERFECT);
+        wordCloud.setPadding(2);
+        wordCloud.setBackground(new CircleBackground(300));
+        wordCloud.setColorPalette(
+            new ColorPalette(
+                new Color(0x4055F1),
+                new Color(0x408DF1),
+                new Color(0x40AAF1),
+                new Color(0x40C5F1),
+                new Color(0x40D3F1),
+                new Color(0xFFFFFF)));
+        wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
+        return wordCloud;
       }
     };
   }
